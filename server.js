216331fb3684ab747a41e8b887fe531759407a59
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-31 12:03:17 -0700

    added acceptance criteria for reference, and starter basic module requiring in server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+/* 
+- [ ] `movie_db` is created and contains a `movies` and `reviews` table.
+- [ ] `movie_db` has been seeded with data.
+- [ ] the `/api/add-movie` POST route successfully adds a movie when tested using Insomnia. (post)
+- [ ] the `/api/movies` GET route renders a list of all movies. (get)
+- [ ] the `/api/movie/:id` DELETE route deletes a route when tested using Insomnia. (delete)
+- [ ] the `/api/movie-reviews` GET & JOIN route successfully gets all movies and the associated reiviews. (get)(join)
+- [ ] the `/api/review/:id` PUT route successfully updates a review pertaining to a specific movie. (put)
+*/
+
+const express = require("express");
+const { appendFile } = require("fs");
+const mysql = require("mysql2");
+const api = require('./routes/index.js');
+const PORT = 3001;
+const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'movie_db',
+},
+    console.log('Connected to movie_db')
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
